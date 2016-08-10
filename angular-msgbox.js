38375b290e8f5974a48a320baeeb5af2519778b4
dunso <admin@dun.so>
2016-08-10 15:55:58 +0800

    modify tpl and add font-size

--- angular-msgbox.js
+++ angular-msgbox.js
@@ -1,37 +1,54 @@
 angular.module("msgbox", ["ui.bootstrap"])
-.factory("$msgbox", function($rootScope, $modal, $q){
-    var success_tpl = '<div class="modal-header" data-ng-show="{{title.length}}"> <h4 class="modal-title">{{title}}</h4></div><div class="modal-body" style="font-size: 18px">{{message}}</div><div class="modal-footer"> <button class="btn btn-primary" data-ng-click="ok($event)">Ok</button> <button class="btn btn-default" data-ng-click="cancel($event)">Cancel</button></div>';
-    
-    var scope = $rootScope.$new();
-    
-    return {
-        show: function(msg, opts){
-            var defer = $q.defer();
-            opts = opts || {};
-            
-            $modal.open({
-                template: success_tpl,
-                scope: scope,
-                controller: function($scope, $modalInstance){
-                    $scope.title = opts.title;
-                    $scope.message = msg;
-                    
-                    $scope.ok = function($event){
-                        $event.preventDefault();
-                        $modalInstance.close();
-                        defer.resolve();
-                    };
-                    $scope.cancel = function($event){
-                        $event.preventDefault();
-                        $modalInstance.close();
-                        defer.reject();
-                    };
-                },
-                size: "sm",
-                backdrop: "static",
-                keyboard: false
-            });
-            return defer.promise;
+    .factory("$msgbox", function($rootScope, $modal, $q){
+        var prompt_tpl = '<div class="modal-header" data-ng-show="{{title.length}}"> <h4 class="modal-title">{{title}}</h4></div><div class="modal-body text-center" style="font-size: 18px"><br> {{message}}<br> <br> <button class="btn cs-button-color" data-ng-click="ok($event)">Ok</button> &nbsp;&nbsp; <button class="btn btn-default" data-ng-click="cancel($event)">Cancel</button> <br> &nbsp;</div>';
+        var alert_tpl = '<div class="modal-header" data-ng-show="{{title.length}}"> <h4 class="modal-title">{{title}}</h4></div><div class="modal-body text-center" style="font-size: 18px"><br> {{message}} <br> <br> <button class="btn cs-button-color" data-ng-click="ok($event)">Ok</button> <br> &nbsp; </div>';
+
+        var scope = $rootScope.$new();
+
+        return {
+            show: function(msg, opts){
+                var defer = $q.defer();
+                opts = opts || {};
+                var tpl = undefined;
+                if(opts.type){
+                    if(opts.type=='alert'){
+                        tpl = alert_tpl;
+                    }
+                    if(opts.type=='prompt'){
+                        tpl = prompt_tpl;
+                    }
+                } else {
+                    tpl = prompt_tpl;
+                }
+                var size = undefined;
+                if(opts.size){
+                    size = opts.size;
+                } else {
+                    size = 'sm';
+                }
+                $modal.open({
+                    template: tpl,
+                    scope: scope,
+                    controller: function($scope, $modalInstance){
+                        $scope.title = opts.title;
+                        $scope.message = msg;
+
+                        $scope.ok = function($event){
+                            $event.preventDefault();
+                            $modalInstance.close();
+                            defer.resolve();
+                        };
+                        $scope.cancel = function($event){
+                            $event.preventDefault();
+                            $modalInstance.close();
+                            defer.reject();
+                        };
+                    },
+                    size: size,
+                    backdrop: "static",
+                    keyboard: false
+                });
+                return defer.promise;
+            }
         }
-    }    
-});
+    });
